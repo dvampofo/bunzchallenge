f8da2d7a349bc4781054274c856daff09bd3f9d7
David Ampofo <davidamp@my.yorku.ca>
2019-03-11 13:29:12 -0400

    Updates

---
+++ precache-manifest.93db527bc4a9ea470dd8bed97f2925c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "32cccb868a2b1138e4d9",
+    "url": "/bunzchallenge/static/js/runtime~main.62193f5a.js"
+  },
+  {
+    "revision": "2bd8962ddd4c40ebe728",
+    "url": "/bunzchallenge/static/js/main.5b8eb459.chunk.js"
+  },
+  {
+    "revision": "2f5c07a45a3c18190641",
+    "url": "/bunzchallenge/static/js/2.c293a13b.chunk.js"
+  },
+  {
+    "revision": "2bd8962ddd4c40ebe728",
+    "url": "/bunzchallenge/static/css/main.41c0d45d.chunk.css"
+  },
+  {
+    "revision": "2f5c07a45a3c18190641",
+    "url": "/bunzchallenge/static/css/2.566c3e31.chunk.css"
+  },
+  {
+    "revision": "f118699aca26912d23c3053d89e6a4d6",
+    "url": "/bunzchallenge/index.html"
+  }
+];
